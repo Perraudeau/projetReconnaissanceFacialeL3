bf57883387862ec8d30665c3ae91a7997a6f598e
Perraudeau Victor <perraudeau.victor@gmail.com>
2014-01-06 17:52:49 +0100

    prémices de la connection à la base de données

---
+++ src/projetreconnaissancefaciale/Modeles/ConnexionBdd.java
@@ -0,0 +1,33 @@
+package projetreconnaissancefaciale.Modeles;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Connection a la base de donnee en singleton
+ *
+ * @author perraudeau
+ */
+public class ConnexionBdd {
+    /**
+     * Informations de connection a la base de donnee
+     */
+    private static final String url = "jdbc:mysql://localhost:3306/reconnaissancefaciale";
+    private static final String user = "root";
+    private static final String password = "";
+    private static Connection connect;
+
+    public static Connection getInstance() {
+        if (connect == null) {
+            try {
+                connect = DriverManager.getConnection(url, user, password);
+            } catch (SQLException ex) {
+                Logger.getLogger(ConnexionBdd.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return connect;
+    }
+}
--- src/projetreconnaissancefaciale/main.java
+++ src/projetreconnaissancefaciale/main.java
@@ -1,11 +1,18 @@
 package projetreconnaissancefaciale;
 
 import java.awt.Color;
-import javax.swing.JFrame;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import projetreconnaissancefaciale.Modeles.ConnexionBdd;
+import projetreconnaissancefaciale.Vues.Connexion;
 
 /**
  *
- * @author Perraudeau Victor
+ * @author perraudeau
  */
 public class main {
 
@@ -13,10 +20,18 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Connexion frame = new Connexion(); 
-        frame.getContentPane().setBackground(new Color(226,226,226));
+        try {
+            Statement state = (Statement) ConnexionBdd.getInstance().createStatement();
+            ResultSet result = state.executeQuery("SELECT * FROM utilisateur");
+            ResultSetMetaData resultMeta = (ResultSetMetaData) result.getMetaData();
+            System.out.println(resultMeta);
+        } catch (SQLException ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Connexion frame = new Connexion();
+        frame.getContentPane().setBackground(new Color(226, 226, 226));
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        
+
     }
 }
